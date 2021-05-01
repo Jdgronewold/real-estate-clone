40340646bd2327f2db80f2f2c113c05695f2462e
Jdgronewold <jeff.gronewold@gmail.com>
2021-05-01 17:15:44 -0600

    basic auth implemented, need to add roles

---
+++ utils/initAuth.ts
@@ -0,0 +1,46 @@
+import { init } from 'next-firebase-auth'
+
+const TWELVE_DAYS_IN_MS = 12 * 60 * 60 * 24 * 1000
+
+const initAuth = () => {
+  init({
+    authPageURL: '/auth',
+    appPageURL: '/',
+    loginAPIEndpoint: '/api/login', // required
+    logoutAPIEndpoint: '/api/logout', // required
+    // Required in most cases.
+    firebaseAdminInitConfig: {
+      credential: {
+        projectId: 'toptalrealestate',
+        clientEmail: 'firebase-adminsdk-c3cpj@toptalrealestate.iam.gserviceaccount.com',
+        // The private key must not be accesssible on the client side.
+        privateKey: process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY) : undefined,
+      },
+      databaseURL: 'https://toptalrealestate-default-rtdb.firebaseio.com/',
+    },
+    firebaseClientInitConfig: {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    },
+    cookies: {
+      name: 'ToptalRealEstate', // required
+      // Keys are required unless you set `signed` to `false`.
+      // The keys cannot be accessible on the client side.
+      keys: [
+        process.env.COOKIE_SECRET_CURRENT,
+        process.env.COOKIE_SECRET_PREVIOUS,
+      ],
+      httpOnly: true,
+      maxAge: TWELVE_DAYS_IN_MS,
+      overwrite: true,
+      path: '/',
+      sameSite: 'strict',
+      secure: true, // set this to false in local (non-HTTPS) development
+      signed: true,
+    },
+  })
+}
+
+export default initAuth
